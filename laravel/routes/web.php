5be1ffd431f7680f821875655e6119ecf8f42521
Claudiu <clvl@fp.insjoaquimmir.cat>
2023-10-24 17:22:10 +0200

    Part 6 de missatges flash (Acabada)

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    $message = 'Loading welcome page';
+    Log::info($message);
+    $request->session()->flash('info', $message);
     return view('welcome');
-});
+ }); 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
