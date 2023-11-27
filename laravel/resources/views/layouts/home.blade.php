c08d73e906fff22e2ad867f83ce4616cb9e04e41
cvlasie <clvl@fp.insjoaquimmir.cat>
2023-11-27 17:57:49 +0100

    M9 Home a medias

--- laravel/resources/views/layouts/home.blade.php
+++ laravel/resources/views/layouts/home.blade.php
@@ -1,29 +1,28 @@
 <x-geomir-layout class="bg-fons-principal">
-    <header class="bg-taronja p-6 mb-4 relative flex items-center justify-between">
+    <header class="bg-taronja p-6 relative flex items-center justify-between">
         <div class="flex items-center">
             <h1 class="text-2xl font-extrabold text-fons-principal font-montserrat text-taronja">GeoPic</h1>
         </div>
-        <img src="{{ asset('img/heart.svg') }}" class="w-6 h-6" alt="Heart Icon" />
+        <img src="{{ asset('img/heart.svg') }}" class="w-6 h-6"/>
     </header>
 
-    <body>
-        <div class="bg-fons-principal p-8">
-            <!-- Contenido de la aplicación -->
+    <body class="bg-fons-principal">
+        <div class="bg-fons-principal p-6">
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <img src="{{ asset('img/pic.png') }}" class="w-8 h-8 rounded-full" alt="User Image" />
-                    <div class="ml-2 text-fons-secundari">
-                        <p class="font-bold">ies.joaquimmir_</p>
-                        <p class="font-bold">40.765475392857844, -73.97644553247858</p>
+                    <div class="ml-2">
+                        <p class="font-bold text-fons-secundari text-sm">ies.joaquimmir_</p>
+                        <div class="font-bold text-fons-secundari text-sm">40.765475392857844, -73.97644553247858</div>
                     </div>
                 </div>
-                <img src="{{ asset('img/options.svg') }}" class="" alt="Options Icon" />
+                <img src="{{ asset('img/options.svg') }}" class="w-6 h-6" alt="Options Icon" />
             </div>
         </div>
+        <img src="{{ asset('img/postimg.png') }}" class="w-full h-auto mb-6" alt="Post Image" />
     </body>
 
     <style>
-        /* Definición de colores personalizados */
         :root {
             --color-taronja: #FF801F;
             --color-taronja-clar: #FF9E1F;
@@ -32,7 +31,6 @@
             --color-fons-secundari: #2A0052;
         }
 
-        /* Estilos con colores personalizados */
         .bg-taronja {
             background-color: var(--color-taronja);
         }
