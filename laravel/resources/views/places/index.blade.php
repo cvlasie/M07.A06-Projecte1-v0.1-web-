ee5f06e700b35a48277242ad4e2228e4b05bc2db
marc <marclmamp@gmail.com>
2023-11-28 20:09:01 +0100

    Part 2 places

--- laravel/resources/views/places/index.blade.php
+++ laravel/resources/views/places/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.box-app')
 
 @section('box-title')
-    {{ __('Places') }}
+    <h1 class="text-2xl font-roboto text-deep-purple">{{ __('Places') }}</h1>
 @endsection
 
 @php
@@ -20,21 +20,23 @@
 
 @section('box-content')
     <!-- Results -->
-    <x-table-index :cols=$cols :rows=$places 
-        :enableActions=true parentRoute='places'
-        :enableSearch=true :search=$search />
+    <div class="bg-cream p-4">
+        <x-table-index :cols="$cols" :rows="$places" 
+            :enableActions="true" parentRoute="places"
+            :enableSearch="true" :search="$search" />
+    </div>
     <!-- Pagination -->
     <div class="mt-8">
         {{ $places->links() }}
     </div>
     <!-- Buttons -->
-    <div class="mt-8">
+    <div class="mt-8 flex space-x-4">
         @can('create', App\Models\Place::class)
-            <x-primary-button href="{{ route('places.create') }}">
+            <x-primary-button href="{{ route('places.create') }}" class="bg-orange text-deep-purple">
                 {{ __('Add new place') }}
             </x-primary-button>
         @endcan
-        <x-secondary-button href="{{ route('dashboard') }}">
+        <x-secondary-button href="{{ route('dashboard') }}" class="bg-light-orange text-deep-purple">
             {{ __('Back to dashboard') }}
         </x-secondary-button>
     </div>
