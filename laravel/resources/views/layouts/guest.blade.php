d42b6f9dbc653ea47e5b08de3082412917f8003e
cvlasie <clvl@fp.insjoaquimmir.cat>
2024-02-20 16:47:56 +0100

    test production

--- laravel/resources/views/layouts/guest.blade.php
+++ laravel/resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles and scripts -->
-        @env(['local','development'])
+        @env(['local','production'])
             @vite(['resources/css/app.css', 'resources/js/app.js'])  
         @endenv
         @env(['production'])
