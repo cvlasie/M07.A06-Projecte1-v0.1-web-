daf8fa816b01cbee7118baa6fa1210a708816df0
cvlasie <clvl@fp.insjoaquimmir.cat>
2024-02-20 16:48:29 +0100

    volvemos al dev de guest

--- laravel/resources/views/layouts/guest.blade.php
+++ laravel/resources/views/layouts/guest.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Styles and scripts -->
-        @env(['local','production'])
+        @env(['local','development'])
             @vite(['resources/css/app.css', 'resources/js/app.js'])  
         @endenv
         @env(['production'])
